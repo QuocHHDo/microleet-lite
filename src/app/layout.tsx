397530352de69b087a5bf5cf5a15daa8d32505d7
Quoc Do <qdo000@citymail.cuny.edu>
2024-10-06 01:18:40 -0500

    Deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-          <Navbar />
-          <main>{children}</main>
+        <Navbar />
+        <main>{children}</main>
       </body>
     </html>
   );
